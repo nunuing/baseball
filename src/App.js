85c169860336a68674957b3cb2459318732c5493
eycho <chw3872@gmail.com>
2023-10-25 23:22:13 +0900

    join page design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const imgURL = '/img/source/symbols/';
   return (
     <h1>
       <ul>
@@ -10,10 +10,10 @@ function App() {
           <input type='button' value='CHECK' />
         </li>
         <li name='pw'>PW
-          <input type='text' />
+          <input type='password' />
         </li>
         <li name='pwCheck'>PW Confirm
-          <input type='text' />
+          <input type='password' />
           <span name='pwConfirm'>Mismatched!</span>
         </li>
         <li name='nickname'>Nickname
@@ -21,17 +21,24 @@ function App() {
           <input type='button' value='CHECK' />
         </li>
         <li name='team'>Team
-          <input type='radio' name='teamchek' value='Tigers' />
-          <input type='radio' name='teamchek' value='Lions'/>
-          <input type='radio' name='teamchek' value='Bears'/>
-          <input type='radio' name='teamchek' value='Landers'/>
-          <input type='radio' name='teamchek' value='Twins'/>
-          <input type='radio' name='teamchek' value='Giants'/>
-          <input type='radio' name='teamchek' value='Eagles'/>
-          <input type='radio' name='teamchek' value='Dinos'/>
-          <input type='radio' name='teamchek' value='Wiz'/>
-          <input type='radio' name='teamchek' value='Heros'/>
+          <input type='radio' name='teamchek' value='Tigers' /> <img src={process.env.PUBLIC_URL + imgURL + 'tigers.png'} alt='tigers' />
+          <input type='radio' name='teamchek' value='Lions' /> <img src={process.env.PUBLIC_URL + imgURL + 'lions.png'} alt='lions' />
+          <input type='radio' name='teamchek' value='Bears' /> <img src={process.env.PUBLIC_URL + imgURL + 'bears.png'} alt='bears' />
+          <input type='radio' name='teamchek' value='Landers' /><img src={process.env.PUBLIC_URL + imgURL + 'landers.png'} alt='landers' />
+          <input type='radio' name='teamchek' value='Twins' /><img src={process.env.PUBLIC_URL + imgURL + 'twins.png'} alt='twins' />
+          <input type='radio' name='teamchek' value='Giants' /><img src={process.env.PUBLIC_URL + imgURL + 'giants.png'} alt='giants' />
+          <input type='radio' name='teamchek' value='Eagles' /><img src={process.env.PUBLIC_URL + imgURL + 'eagles.png'} alt='eagles' />
+          <input type='radio' name='teamchek' value='Dinos' /><img src={process.env.PUBLIC_URL + imgURL + 'dinos.png'} alt='dinos' />
+          <input type='radio' name='teamchek' value='Wiz' /><img src={process.env.PUBLIC_URL + imgURL + 'wiz.png'} alt='wiz' />
+          <input type='radio' name='teamchek' value='Heros' /><img src={process.env.PUBLIC_URL + imgURL + 'heros.png'} alt='heros' />
         </li>
+        <li name='profile'>profile
+          <input type='url' />
+          <input type='button' value='attach' />
+          <img name='default' src={process.env.PUBLIC_URL + 'img/default.png'} alt='default' />
+        </li>
+        <input type='submit' value='Submit'/> 
+        <input type='button' value='Cancle'/>
       </ul>
     </h1>
   )
