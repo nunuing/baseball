f158b07df364fb7795a0bad5930a3c4f6f0cd3ad
eycho <chw3872@gmail.com>
2023-10-25 23:54:09 +0900

    css

--- src/App.js
+++ src/App.js
@@ -21,16 +21,16 @@ function App() {
           <input type='button' value='CHECK' />
         </li>
         <li name='team'>Team
-          <input type='radio' name='teamchek' value='Tigers' /> <img src={process.env.PUBLIC_URL + imgURL + 'tigers.png'} alt='tigers' />
-          <input type='radio' name='teamchek' value='Lions' /> <img src={process.env.PUBLIC_URL + imgURL + 'lions.png'} alt='lions' />
-          <input type='radio' name='teamchek' value='Bears' /> <img src={process.env.PUBLIC_URL + imgURL + 'bears.png'} alt='bears' />
-          <input type='radio' name='teamchek' value='Landers' /><img src={process.env.PUBLIC_URL + imgURL + 'landers.png'} alt='landers' />
-          <input type='radio' name='teamchek' value='Twins' /><img src={process.env.PUBLIC_URL + imgURL + 'twins.png'} alt='twins' />
-          <input type='radio' name='teamchek' value='Giants' /><img src={process.env.PUBLIC_URL + imgURL + 'giants.png'} alt='giants' />
-          <input type='radio' name='teamchek' value='Eagles' /><img src={process.env.PUBLIC_URL + imgURL + 'eagles.png'} alt='eagles' />
-          <input type='radio' name='teamchek' value='Dinos' /><img src={process.env.PUBLIC_URL + imgURL + 'dinos.png'} alt='dinos' />
-          <input type='radio' name='teamchek' value='Wiz' /><img src={process.env.PUBLIC_URL + imgURL + 'wiz.png'} alt='wiz' />
-          <input type='radio' name='teamchek' value='Heros' /><img src={process.env.PUBLIC_URL + imgURL + 'heros.png'} alt='heros' />
+          <input type='radio' name='teamchek' value='Tigers' /> <img className='symbols' src={process.env.PUBLIC_URL + imgURL + 'tigers.png'} alt='tigers' />
+          <input type='radio' name='teamchek' value='Lions' /> <img className='symbols' src={process.env.PUBLIC_URL + imgURL + 'lions.png'} alt='lions' />
+          <input type='radio' name='teamchek' value='Bears' /> <img className='symbols' src={process.env.PUBLIC_URL + imgURL + 'bears.png'} alt='bears' />
+          <input type='radio' name='teamchek' value='Landers' /><img className='symbols' src={process.env.PUBLIC_URL + imgURL + 'landers.png'} alt='landers' />
+          <input type='radio' name='teamchek' value='Twins' /><img className='symbols' src={process.env.PUBLIC_URL + imgURL + 'twins.png'} alt='twins' />
+          <input type='radio' name='teamchek' value='Giants' /><img className='symbols' src={process.env.PUBLIC_URL + imgURL + 'giants.png'} alt='giants' />
+          <input type='radio' name='teamchek' value='Eagles' /><img className='symbols' src={process.env.PUBLIC_URL + imgURL + 'eagles.png'} alt='eagles' />
+          <input type='radio' name='teamchek' value='Dinos' /><img className='symbols' src={process.env.PUBLIC_URL + imgURL + 'dinos.png'} alt='dinos' />
+          <input type='radio' name='teamchek' value='Wiz' /><img className='symbols' src={process.env.PUBLIC_URL + imgURL + 'wiz.png'} alt='wiz' />
+          <input type='radio' name='teamchek' value='Heros' /><img className='symbols' src={process.env.PUBLIC_URL + imgURL + 'heros.png'} alt='heros' />
         </li>
         <li name='profile'>profile
           <input type='url' />
