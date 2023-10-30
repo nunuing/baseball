ddc23210c102fc6dc94253aace201dd85c90bcfa
eycho <chw3872@gmail.com>
2023-10-30 14:54:53 +0900

    pw checking fuction-error occured

--- src/RegisterPage.js
+++ src/RegisterPage.js
@@ -1,71 +1,76 @@
 import { useState } from 'react';
 import './RegisterPage.css';
-function IdForm() {
+function RegisterPage() {
+  const imgURL = '/img/source/symbols/';
+
   const [id, setId] = useState("");
   const [idMessage, setIdMessge] = useState("");
   const [validId, setvalidId] = useState(false);
 
-  return (
-    <div>
-      <label htmlFor='id'>ID </label>
-      <input type='text' id='id' name='id' value={id} onChange={event => {
-        const currentId = event.target.value;
-        setId(currentId);
-
-        const idRegExp = /^[a-zA-z0-9]{4,12}$/;
-
-        if (!idRegExp.test(currentId)) {
-          setIdMessge("invalid ID!");
-          setvalidId(false);
-        }
-        else {
-          setIdMessge("valid ID!");
-          setvalidId(true);
-        }
-      }} />
-      <p className='message'>{idMessage}</p>
-    </div>
-  );
-}
-
-function PwForm() {
   const [pw, setPw] = useState("");
   const [pwMessage, setPwMessage] = useState("");
   const [validPw, setValidPw] = useState(false);
-  
-  return (
-    <div>
-      <label name='pw' htmlFor='pw'>PW </label>
-      <input type='password' id='pw' value={pw} onChange={event => {
-        const currentPw = event.target.value;
-        setPw(currentPw);
 
-        const pwRegExp = /([0-9]|[a-z]|[A-Z]){4,20}/
-        if (!pwRegExp.test(currentPw)) {
-          setPwMessage("invalid PW!");
-          setValidPw(false);
-        }
-        else {
-          setPwMessage("valid PW!");
-          setValidPw(true);
-        }
-      }} />
-      <p className='message'>{pwMessage}</p>
-    </div>
-  );
-}
-function RegisterPage() {
-  const imgURL = '/img/source/symbols/';
+  const [pwCheck, setPwCheck] = useState("");
+  const [pwCheckMessage, setPwCheckMessage] = useState("");
+  const [validPwCheck, setValidPwCheck] = useState(false);
+
+  const handleIdInput = e => {
+    const currentId = e.target.value;
+    setId(currentId);
+
+    const idRegExp = /^[a-zA-z0-9]{4,12}$/;
+
+    if (!idRegExp.test(currentId)) {
+      setIdMessge("invalid ID!");
+      setvalidId(false);
+    }
+    else {
+      setIdMessge("valid ID!");
+      setvalidId(true);
+    }
+  };
+
+  const handlePwInput = e => {
+    const currentPw = e.target.value;
+    setPw(currentPw);
+
+    const pwRegExp = /([0-9]|[a-z]|[A-Z]){4,20}/
+    if (!pwRegExp.test(currentPw)) {
+      setPwMessage("invalid PW!");
+      setValidPw(false);
+    }
+    else {
+      setPwMessage("valid PW!");
+      setValidPw(true);
+    }
+  };
+
+  const handlePwCheckInput = e => {
+    if (pw === e.target.value) {
+      pwCheckMessage = "matched!";
+    }
+    else {
+      pwCheckMessage = "mismatched!";
+    }
+  };
 
   return (
     <h1>
-
-      <IdForm></IdForm>
-      <PwForm></PwForm>
+      <div>
+        <label htmlFor='id'>ID </label>
+        <input type='text' id='id' name='id' value={id} onChange={handleIdInput} />
+        <p className='message'>{idMessage}</p>
+      </div>
+      <div>
+        <label name='pw' htmlFor='pw'>PW </label>
+        <input type='password' id='pw' value={pw} onChange={handlePwInput}/>
+        <p className='message'>{pwMessage}</p>
+      </div>
       <div>
         <label name='pwCheck' htmlFor='pwCheck'>PW check </label>
-        <input type='password' />
-        <span name='pwConfirm'>Mismatched!</span>
+        <input type='password' id='pwChek' value={pwCheck} onChange={handlePwCheckInput} />
+        <p name='pwConfirm'>{pwCheckMessage}</p>
       </div>
       <div>
         <label name='nickname' htmlFor='nickname'>Nickname </label>
