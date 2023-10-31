5b4afb6ebbc3c4659b74d0c7a4f513bbe74bcfd3
eycho <chw3872@gmail.com>
2023-10-31 19:05:35 +0900

    design changes

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,6 +1,34 @@
 import './HomePage.css';
-import TitleBar from './TitleBar';
-import HomeContent from './HomeContent';
+import { Link } from 'react-router-dom';
+import RegisterPage from './RegisterPage';
+
+function TitleBar() {
+    return (
+        <div id='title_bar'>
+            <div className='background'>
+                <img id='cloud1' src='/img/cloud.png' />
+                <img id='cloud2' src='/img/cloud.png' />
+                <img id='cloud3' src='/img/cloud.png' />
+                <img id='cloud4' src='/img/cloud.png' />
+            </div>
+            <div className='title_navi'>
+                <Link className='link' to='/board'>Board</Link>
+                <Link className='link' to='/game'>Game</Link>
+                <img id='title_img' src='/img/Title_heart.png' />
+                <Link className='link' to='/login'>Log In</Link>
+                <Link className='link' to='/join'>Join</Link>
+            </div>
+        </div>
+    );
+}
+
+function HomeContent(props) {
+    return(
+        <div className='content_background'>
+            <RegisterPage></RegisterPage>
+        </div>
+    );
+}
 
 function HomePage() {
     return (
