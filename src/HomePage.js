e0acc78306f4ea1308427dff7c40d8c84b41f12f
eycho <chw3872@gmail.com>
2023-11-04 12:56:37 +0900

    main page designing

--- src/HomePage.js
+++ src/HomePage.js
@@ -7,7 +7,16 @@ function ScoreBoard() {
     const scoreList = list_score.map((score, index) => <li className='board_info' id={'score'+index} key={index} >{score}</li>);
     return scoreList;
 }
+function GameInfo() {
+    const infos = ["R", "H", "B", "E", "6", "11", "8", "0", "3", "7", "3", "1"];
+    const infoList = infos.map((info, index) => <li className='infos' key={index}>{info}</li>);
 
+    return <ul className='game_info'>{infoList}</ul>;
+}
+
+function BallInfo() {
+   
+}
 function HomePage() {
     return (
         <div className='main'>
@@ -17,6 +26,9 @@ function HomePage() {
                     <div className='content_background'>
                         <ul className='score_board'>
                             <ScoreBoard></ScoreBoard>
+                            <div className='board_bottom'>
+                                <GameInfo></GameInfo>
+                            </div>
                         </ul>
                     </div>
                 </div>
