6b2b532b27987c8856be09e70cf9434bbe929f1d
eycho <chw3872@gmail.com>
2023-11-07 22:32:05 +0900

    ready board page

--- src/Board.js
+++ src/Board.js
@@ -2,10 +2,19 @@ import './Board.css'
 import TitleBar from './TitleBar';
 
 function Board() {
-    return(
-        <div>
+    return (
+        <div className='main'>
             <TitleBar state={'board'} ></TitleBar>
-            Board Page!
+            <div className='board_main'>
+                <div className='board_ground'>
+                    <div className='board_background'>
+                        <div className='board_content'>
+                            <div className='board_inputs'>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
